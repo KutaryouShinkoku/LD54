596c8a22905b10cc94c0187a4a48e50e8fb1ea97
ihcanan <549805669@qq.com>
2023-10-01 23:01:07 -0500

    主界面UI、字体上传

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject _uICredits;
+    [SerializeField] GameObject _uISettings;
+    [SerializeField] Slider _volumeSlider;
+
+    private bool isSettingOpen;
+
+    public void Start()
+    {
+        isSettingOpen = false;
+    }
+    //开始游戏
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    //结束游戏
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    //打开Credits
+    public void ShowCredits()
+    {
+        _uICredits.SetActive(true);
+    }
+    //关闭Credits
+    public void CloseCredits()
+    {
+        _uICredits.SetActive(false);
+    }
+    //打开或关闭设置
+    public void ShowSettings()
+    {
+        if(isSettingOpen == true)
+        {
+            _uISettings.SetActive(false);
+            isSettingOpen = false;
+        }
+        else
+        {
+            _uISettings.SetActive(true);
+            isSettingOpen = true;
+        }
+    }
+
+}
